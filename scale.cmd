999977353ac8615d7b713037c13b53602e4133c6
plc-user <74435298+plc-user@users.noreply.github.com>
2021-06-02 21:21:11 +0200

    Added Batch-File for recursive scaling of elements
    Read Comments in "rem"-Lines to modify Batch-File for your needs.

---
+++ scale.cmd
@@ -0,0 +1,37 @@
+@echo skaliere QET - Element-Dateien ...
+
+@echo off & setlocal
+
+rem Wo liegt das Programm?
+rem Where does the Program lie?
+set "ProgPfad=C:\Tools\QET_ElementScaler"
+
+rem Wo liegen die Element-Dateien?
+rem Where do the element-files reside?
+set "DataPfad=Z:\win\Elemente"
+
+rem Welcher Faktor soll angewendet werden?
+set "Skalierung=0.9"
+
+
+rem ------  Ab hier nichts mehr ändern!  ------
+rem ------  No changes below this line!  ------
+
+
+rem Findet rekursiv alle passenden Dateien:
+@for /r "%DataPfad%" %%i in ("*.elmt") do (
+    rem gefundene Dateien bearbeiten:
+    "%ProgPfad%\QET_ElementScaler.exe" "%%i" %Skalierung%
+    )
+
+
+rem Rekursiv den XML-Header entfernen:
+@for /r "%DataPfad%" %%i in ("*.SCALED.elmt") do (
+    rem gefundene Dateien bearbeiten:
+    more +1 < "%%i" > "%%i.txt"
+    rem und wieder umbenennen:
+    move /Y "%%i.txt" "%%i"
+    )
+
+
+@echo + + + Fertig! + + +
