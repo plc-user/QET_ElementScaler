4f4bda37169ac18041b3a27473e4fa74c5c42e50
plc-user <74435298+plc-user@users.noreply.github.com>
2022-10-09 13:30:16 +0200

    added creation of subdirs to scripts

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,7 @@
-rem compile with GCC in "path"
+rem create sub-directories:
+md obj\inc\pugixml > nul 2> nul
 
+rem compile with GCC in "path"
 g++.exe -Wall -O2 -std=c++17 -c inc\pugixml\pugixml.cpp -o obj\inc\pugixml\pugixml.o
 g++.exe -Wall -O2 -std=c++17 -c main.cpp -o obj\main.o
 g++.exe -o QET_ElementScaler.exe obj\inc\pugixml\pugixml.o obj\main.o -s
--- compile.sh
+++ compile.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# compile QET_ElementScaler
+# create sub-directories
+mkdir -p obj/inc/pugixml
 
-g++ -Wall -O2 std=c++17 -c inc/pugixml/pugixml.cpp -o obj/inc/pugixml/pugixml.o
-g++ -Wall -O2 std=c++17 -c main.cpp -o obj/main.o
+
+# compile QET_ElementScaler
+g++ -Wall -O2 -std=c++17 -c inc/pugixml/pugixml.cpp -o obj/inc/pugixml/pugixml.o
+g++ -Wall -O2 -std=c++17 -c main.cpp -o obj/main.o
 g++ -o  QET_ElementScaler obj/inc/pugixml/pugixml.o obj/main.o  -s
