2327064f96b807938d84baa3a3c9dc6f136a9d7a
plc-user <74435298+plc-user@users.noreply.github.com>
2024-05-20 23:46:11 +0200

    adjust version-number

--- main.h
+++ main.h
@@ -42,7 +42,7 @@
 // global variables
 // ============================================================================
 
-const std::string sVersion = "v0.5.0beta17";
+const std::string sVersion = "v0.5.0beta18";
 
 // the element-file to process:
 static std::string ElementFile       = "";
