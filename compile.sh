312ac132dc98b8793556667a40a67d21560cc87d
plc-user <74435298+plc-user@users.noreply.github.com>
2024-11-26 08:20:09 +0100

    small change in compile-script

--- compile.sh
+++ compile.sh
@@ -14,21 +14,22 @@ echo ""
 echo ""
 echo "--------------------------------------------------------------------------------"
 
+Compiler="g++"
 CompileOptions="-Wall -std=c++17 -O2"
 
 # compile
-g++ $CompileOptions  -c inc/pugixml/pugixml.cpp -o obj/inc/pugixml/pugixml.o
-g++ $CompileOptions  -c inc/helpers.cpp         -o obj/inc/helpers.o
-g++ $CompileOptions  -c inc/elements.cpp        -o obj/inc/elements.o
-g++ $CompileOptions  -c main.cpp                -o obj/main.o
+$Compiler $CompileOptions -c inc/pugixml/pugixml.cpp -o obj/inc/pugixml/pugixml.o
+$Compiler $CompileOptions -c inc/helpers.cpp         -o obj/inc/helpers.o
+$Compiler $CompileOptions -c inc/elements.cpp        -o obj/inc/elements.o
+$Compiler $CompileOptions -c main.cpp                -o obj/main.o
 
 # link:
-g++  -o QET_ElementScaler \
-        obj/inc/pugixml/pugixml.o \
-        obj/inc/helpers.o \
-        obj/inc/elements.o \
-        obj/main.o  \
-     -s
+$Compiler -o QET_ElementScaler \
+             obj/inc/pugixml/pugixml.o \
+             obj/inc/helpers.o \
+             obj/inc/elements.o \
+             obj/main.o  \
+          -s
 
 # delete object-files again:
 rm obj/inc/pugixml/*.o  &> /dev/null
